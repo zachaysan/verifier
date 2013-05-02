f7ab85de140358db1acd73eea60e43a0b981eb57
Zach Aysan <zachaysan@gmail.com>
2013-05-02 16:05:20 -0400

    .

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Verifier::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => 'verify#index'
   match '/:k', :controller => 'verify', :action => 'show'
-  # See how all your routes lay out with "rake routes"
+  # Seeo how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
